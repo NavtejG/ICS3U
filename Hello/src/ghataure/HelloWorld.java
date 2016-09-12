d011a5e5062ba42c168a1a6c23f909298b64d312
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-09-12 13:12:32 -0400

    HelloWorld

---
+++ Hello/src/ghataure/HelloWorld.java
@@ -0,0 +1,12 @@
+package ghataure;
+
+/**
+ * Displays a "Hello World!" message on the screen
+ */
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
