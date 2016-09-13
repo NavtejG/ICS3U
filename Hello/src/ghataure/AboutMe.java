9988f9db0bf254373dd50a49628d96ffec37a1eb
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-09-13 13:41:45 -0400

    AboutMe

---
+++ Hello/src/ghataure/AboutMe.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ghataure;
+
+/**
+ * @author 323878314
+ *
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-10s","Navtej.G\n");
+		System.out.format("%-10s","Miss.Kemp\n");
+		System.out.format("%-10s","Runnymede Collegiate Institute\n");
+		System.out.format("%-10s","\"Go Ravens!\"\n");
+           
+	}
+
+}
