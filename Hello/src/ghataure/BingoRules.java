a979c5a01b976464fd19bbe7ee24263958f85d72
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-09-15 13:14:18 -0400

    BingoRules

---
+++ Hello/src/ghataure/BingoRules.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ghataure;
+
+/**
+ *  ICS3U
+ * BingoCard
+ *14/09/2016
+ *Navtej Ghataure
+ */
+public class BingoRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.format("%2s%6s","Bingo","Card\n\n"); 
+	     System.out.print("1. The caller randomly pulls a numbered bingo ball.\n\n");
+	     System.out.print("2. The number is placed on the bingo board and called out.\n\n");
+	     System.out.print("3. Players look for the called number on their bingo card.\n\n");
+	     System.out.print("4. If the number is located, it is marked off.\n\n");
+	     System.out.print("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.\n\n");
+	     System.out.print("6. The winning player yells BINGO.\n\n");
+	}
+
+}
