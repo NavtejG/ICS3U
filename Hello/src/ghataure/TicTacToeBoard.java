9934249e270033816516cb23cc48ae90ca2251f7
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-09-15 13:39:31 -0400

    TicTacToeBoard

---
+++ Hello/src/ghataure/TicTacToeBoard.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ghataure;
+
+/**
+ *  ICS3U
+ * TicTacToeBoard
+ *14/09/2016
+ *Navtej Ghataure
+ *
+ */
+public class TicTacToeBoard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.format("%6s%3s%8s","|"," ","|\n");
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s","---------------------\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n");
+		 System.out.format("%6s%3s%8s","|","X","|\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s","---------------------\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n");
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 
+	}
+
+}
