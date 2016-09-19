c72998e1516bb9e9235443fef60e5a556d81b56f
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-09-19 12:35:21 -0400

    TicTacToe Navtej

---
+++ Hello/src/ghataure/TicTacToeRules.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ghataure;
+
+/**
+ * *  ICS3U
+ * TicTacToeRules
+ *14/09/2016
+ *Navtej Ghataure
+ */
+public class TicTacToeRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.format("%2s%4s%4s%4s","Tic","Tac"," Toe"," Rules\n\n");
+		 System.out.print("Tic Tac Toe is a game for 2 players\n\n");
+	     System.out.print("1. A grid of 9 squares is drawn.\n\n");
+	     System.out.print("2. The first player draws an X in an empty square.\n\n");
+	     System.out.print("3. The second player draws on O in an empty square.\n\n");
+	     System.out.print("4. Steps 2 and 3 are repeated until a Tic Tac pattern is created.\n\n");
+	     System.out.print("    Patterns can be disgonal, horizontal, or vertical.\n\n");
+	     System.out.print("5. A winning pattern is completed by drawing a line through the pattern.\n\n");
+
+	}
+
+}
