97a49e97be5a422630ebbdc651062d6818da50ae
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-09-19 13:00:12 -0400

    JavaTerminology Navtej

---
+++ Hello/src/ghataure/Classschedule.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package ghataure;
+
+/**
+ * ICS3U
+ *Class schedule 
+ *14.09/2016
+ *Navtej Ghataure
+ */
+public class Classschedule {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.format("%-10s","Navtej.G\n");
+		System.out.format("%-10s","Miss.Kemp\n");
+		System.out.format("%-10s","Runnymede Collegiate Institute\n\n");
+		
+		   System.out.format("%-10s%8s%8s","Period","Day 1","\tDay 2\n"); 
+		   System.out.format("%-10s%8s%8s","1","SPH3U1-01","\tSPH3U1-01\n");
+		   System.out.format("%-10s%8S%8s","08:45","Enns,M","\tEnns,M\n");
+		   System.out.format("%-10s%8s%8s","10:02","226","\t226\n\n");	   
+		  
+
+		   System.out.format("%-10s%8s%8s","2","BAF3M/-02","\tBAF3M-02\n");
+		   System.out.format("%-10s%8S%8s","10:07","ZZteacher,H","\tZZteacher,H\n");
+		   System.out.format("%-10s%8s%8s","11:24","220","\t220\n\n");
+		   
+		   System.out.format("%-10s%8s","Lunch","\tLunch\n");
+		   System.out.format("%-10s%8S","11:24","\t11:24\n");
+		   System.out.format("%-10s%8s","12:26","\t12:26\n\n");
+		   
+		   System.out.format("%-10s%8s%8s","3","ICS3U/3U1-01","\tICS3U/3U1-01\n");
+		   System.out.format("%-10s%8S%8s","12:26","Kemp.C","\tKemp.C\n");
+		   System.out.format("%-10s%8s%8s","13:43","024","\t024\n\n");
+
+		   System.out.format("%-10s%8s%8s","4","AWK2/301-01","\tAWK2/301-01\n");
+		   System.out.format("%-10s%8S%8s","13:48","Lambert,A","\tLambert,A\n");
+		   System.out.format("%-10s%8s%8s","15:05","227","\t227\n\n");
+	
+
+	}
+
+}
---
+++ Hello/src/ghataure/JavaTerminology.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ghataure;
+
+/**
+ * ICS3U
+ * JavaTerminology
+ *19/09/2016
+ *Navtej Ghataure
+ *
+ */
+public class JavaTerminology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.format("%2s","Java Terminology\n\n");
+		 System.out.print("Source code:\n");
+		 System.out.print("The code typed by a programmer.\n\n");
+		 System.out.print("Statement:\n");
+		 System.out.print("An instruction in a program.\n\n");
+		 System.out.print("String:\n");
+		 System.out.print("A set of characters.\n\n");
+		 System.out.print("Execute:\n");
+		 System.out.print("To run a program.\n\n");
+		 System.out.print("Class:\n");
+		 System.out.print("The description of an object.\n\n");
+	     
+	}
+
+}
