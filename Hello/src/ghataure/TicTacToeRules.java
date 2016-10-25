271de1d915d916e14f8708719a5cc62a1debb9aa
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-10-25 10:50:00 -0400

    TicTacToe Rules

--- Hello/src/ghataure/TicTacToeRules.java
+++ Hello/src/ghataure/TicTacToeRules.java
@@ -15,14 +15,14 @@ public class TicTacToeRules {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 System.out.print("Tic Tac Toe Rules\n\n");
-		 System.out.print("Tic Tac Toe is a game for 2 players\n\n");
-	     System.out.print("1. A grid of 9 squares is drawn.\n\n");
-	     System.out.print("2. The first player draws an X in an empty square.\n\n");
-	     System.out.print("3. The second player draws on O in an empty square.\n\n");
-	     System.out.print("4. Steps 2 and 3 are repeated until a Tic Tac pattern is created.\n\n");
-	     System.out.print("    Patterns can be disgonal, horizontal, or vertical.\n\n");
-	     System.out.print("5. A winning pattern is completed by drawing a line through the pattern.\n\n");
+		 System.out.println("Tic Tac Toe Rules\n\n");
+		 System.out.println("Tic Tac Toe is a game for 2 players\n\n");
+	     System.out.println("1. A grid of 9 squares is drawn.\n\n");
+	     System.out.println("2. The first player draws an X in an empty square.\n\n");
+	     System.out.println("3. The second player draws on O in an empty square.\n\n");
+	     System.out.println("4. Steps 2 and 3 are repeated until a Tic Tac pattern is created.\n\n");
+	     System.out.println("    Patterns can be disgonal, horizontal, or vertical.\n\n");
+	     System.out.println("5. A winning pattern is completed by drawing a line through the pattern.\n\n");
 
 	}
 
