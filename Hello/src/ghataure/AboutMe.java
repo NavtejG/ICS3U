fb76176a649e1815036b04647bcf907d15e8b9b3
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-10-25 10:54:36 -0400

    BingoRules

--- Hello/src/ghataure/AboutMe.java
+++ Hello/src/ghataure/AboutMe.java
@@ -5,8 +5,8 @@ package ghataure;
 
 /**
  * ICS3U
- * AboutMe
- *14/09/2016 
+ * AboutMe.java
+ *13/09/2016 
  *Navtej Ghataure
  */
 public class AboutMe {
@@ -15,10 +15,10 @@ public class AboutMe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.format("%-10s","Navtej.G\n");
-		System.out.format("%-10s","Miss.Kemp\n");
-		System.out.format("%-10s","Runnymede Collegiate Institute\n");
-		System.out.format("%-10s","\"Go Ravens!\"\n\n");
+		System.out.print("Navtej.G\n");
+		System.out.print("Miss.Kemp\n");
+		System.out.print("Runnymede Collegiate Institute\n");
+		System.out.print("\"Go Ravens!\"\n\n");
 		
 	
 		
