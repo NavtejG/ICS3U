9f15824e5f55e3c71f0107113c40e40820a85956
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-10-26 12:38:25 -0400

    AboutMe

--- Hello/src/ghataure/AboutMe.java
+++ Hello/src/ghataure/AboutMe.java
@@ -6,7 +6,7 @@ package ghataure;
 /**
  * ICS3U
  * AboutMe.java
- * Shows informations about navtej
+ * Shows informations about Navtej
  *13/09/2016 
  *Navtej Ghataure
  */
@@ -23,27 +23,27 @@ public class AboutMe {
 		
 	
 		
-		   System.out.format("%-10s %8s %8s","Period","Day 1","\tDay 2\n"); 
-		   System.out.format("%-10s%8s%8s","1","SPH3U1-01","\tSPH3U1-01\n");
-		   System.out.format("%-10s%8S%8s","08:45","Enns,M","\tEnns,M\n");
-		   System.out.format("%-10s%8s%8s","10:02","226","\t226\n\n");	   
+		   System.out.format("%-10s%1s%15s","Period","Day 1","Day 2\n"); 
+		   System.out.format("%-10s%5s%15s","1","SPH3U1-01","SPH3U1-01\n");
+		   System.out.format("%-10s%5S%15s","08:45","Enns,M","Enns,M\n");
+		   System.out.format("%-10s%5s%15s","10:02","226","226\n\n");	   
 		  
 
-		   System.out.format("%-10s%8s%8s","2","BAF3M/-02","\tBAF3M-02\n");
-		   System.out.format("%-10s%8S%8s","10:07","ZZteacher,H","\tZZteacher,H\n");
-		   System.out.format("%-10s%8s%8s","11:24","220","\t220\n\n");
+		   System.out.format("%-10s%8s%15s","2","BAF3M/-02","BAF3M-02\n");
+		   System.out.format("%-10s%5S%15s","10:07","ZZteacher,H","ZZteacher,H\n");
+		   System.out.format("%-10s%5s%15s","11:24","220","220\n\n");
 		   
-		   System.out.format("%-10s%8s","Lunch","\tLunch\n");
-		   System.out.format("%-10s%8S","11:24","\t11:24\n");
-		   System.out.format("%-10s%8s","12:26","\t12:26\n\n");
+		   System.out.format("%-10s%8s","Lunch","Lunch\n");
+		   System.out.format("%-10s%8S","11:24","11:24\n");
+		   System.out.format("%-10s%8s","12:26","12:26\n\n");
 		   
-		   System.out.format("%-10s%8s%8s","3","ICS3U/3U1-01","\tICS3U/3U1-01\n");
-		   System.out.format("%-10s%8S%8s","12:26","Kemp.C","\tKemp.C\n");
-		   System.out.format("%-10s%8s%8s","13:43","024","\t024\n\n");
+		   System.out.format("%-10s%8s%15s","3","ICS3U/3U1-01","ICS3U/3U1-01\n");
+		   System.out.format("%-10s%5S%15s","12:26","Kemp.C","Kemp.C\n");
+		   System.out.format("%-10s%5s%15s","13:43","024","024\n\n");
 
-		   System.out.format("%-10s%8s%8s","4","AWK2/301-01","\tAWK2/301-01\n");
-		   System.out.format("%-10s%8S%8s","13:48","Lambert,A","\tLambert,A\n");
-		   System.out.format("%-10s%8s%8s","15:05","227","\t227\n\n");
+		   System.out.format("%-10s%8s%15s","4","AWK2/301-01","AWK2/301-01\n");
+		   System.out.format("%-10s%5S%15s","13:48","Lambert,A","Lambert,A\n");
+		   System.out.format("%-10s%5s%15s","15:05","227","227\n\n");
 	
            
 	}
