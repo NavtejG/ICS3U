18aa7504accc70e918f5af334926d922648256de
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-10-31 13:22:12 -0400

    Divisible by 7

---
+++ Hello/src/ghataure/Divisableby7.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package ghataure;
+
+import java.util.Scanner;
+
+/**
+ *ICS3U
+ *Divisableby7
+ *31/10/2016
+ *Navtej Ghataure
+ *
+ *
+ */
+public class Divisableby7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+	
+		int integer;
+		
+		
+
+		{
+			System.out.print("Enter a integer:");
+			integer=scan.nextInt();
+
+		
+			}
+
+			if (integer % 7 != 0)
+				System.out.print("This integer is not divisible by 7");
+			else
+				System.out.print("This integer is divisible by 7");
+			
+
+	}
+}
+
+
