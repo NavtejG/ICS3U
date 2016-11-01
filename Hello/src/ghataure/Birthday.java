a9e4336633b399ca4f25e50d4db062931223c894
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-11-01 12:46:32 -0400

    Birthday

--- Hello/src/ghataure/Birthday.java
+++ Hello/src/ghataure/Birthday.java
@@ -15,33 +15,61 @@ import java.util.Scanner;
  */
 public class Birthday {
 
+
 	/**
-	 * @param args
+	 * 
 	 */
 	public static void main(String[] args) {
 		
 		Scanner scan=new Scanner(System.in);
 		
+		int number;
+		int birthMonth;
+		int birthDay;
 		
-		int number1;
-		double january;
-		double febuary;
-		double march;
-		double april;
-		double may;
-		double june;
-		double july;
-		double august;
-		double september;
-		double october;
-		double november;
-		double december;
+		int january; 
+		int febuary; 
+	    int march;
+		int april; 
+		int  may;
+	    int june;
+		int july;
+		int august;
+		int september;
+		int october;
+	    int november;
+	    int december;
+	     
+	    january=1; 
+	    febuary=2; 
+		march=3;
+		april=4; 
+	    may=5;
+		june=6;
+		july=7;
+		august=8;
+		september=9;
+	    october=10;
+	    november=11;
+	    december=12;
+	    
+	    System.out.print("birthMonth: ");
+	    birthMonth=scan.nextInt();
+	    
+	    System.out.print("birthDay: ");
+	    birthDay=scan.nextInt();
+	    
+	    number=( birthDay+(5*(9+(4*(6+(birthMonth*5))))));
+	    System.out.println("Your number is: " + number);
+	    
+	    System.out.println("Your Bithday is: " + birthMonth +"\\"+ birthDay);
+			
+		}
 		
 	
 		
 		
-				
-		System.out.print("What month is your birthday 1-12?");
+		
 	
 
 
@@ -51,4 +79,4 @@ public class Birthday {
 
 	}
 
-}
+
