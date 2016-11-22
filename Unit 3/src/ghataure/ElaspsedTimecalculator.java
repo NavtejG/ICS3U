26db1bd8a1895b8c6985f5400857bfc9ae5a29a1
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-11-22 12:41:33 -0500

    Elapsed time calculator

--- Unit 3/src/ghataure/ElaspsedTimecalculator.java
+++ Unit 3/src/ghataure/ElaspsedTimecalculator.java
@@ -3,6 +3,9 @@
  */
 package ghataure;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  * @author 323878314
  *
@@ -13,36 +16,38 @@ public class ElaspsedTimecalculator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		import java.util
+		Scanner scan=new Scanner(System.in);
+		
+		int hour  ;
+		String time;
+		int elapsed;
+		double price = 0.00;
+		
+		
 
-		   public int getMonths(GregorianCalendar g1, GregorianCalendar g2) {
-		      int elapsed = 0;
-		      GregorianCalendar gc1, gc2;
-		      if (g2.after(g1)) {
-		         gc2 = (GregorianCalendar) g2.clone();
-		         gc1 = (GregorianCalendar) g1.clone();
-		      }
-		      else   {
-		         gc2 = (GregorianCalendar) g1.clone();
-		         gc1 = (GregorianCalendar) g2.clone();
-		      }
-		      gc1.clear(Calendar.MILLISECOND);
-		      gc1.clear(Calendar.SECOND);
-		      gc1.clear(Calendar.MINUTE);
-		      gc1.clear(Calendar.HOUR_OF_DAY);
-		      gc1.clear(Calendar.DATE);
-		      gc2.clear(Calendar.MILLISECOND);
-		      gc2.clear(Calendar.SECOND);
-		      gc2.clear(Calendar.MINUTE);
-		      gc2.clear(Calendar.HOUR_OF_DAY);
-		      gc2.clear(Calendar.DATE);
-		      while ( gc1.before(gc2) ) {
-		         gc1.add(Calendar.MONTH, 1);
-		         elapsed++;
-		      }
-		      return elapsed;
+		   DecimalFormat df = new DecimalFormat ("0.00");
+			
+			System.out.print("Whats your starting hour?");
+			hour = scan.nextInt();
+			scan.nextLine();
+			
+			System.out.println("Am or Pm?");
+			time = scan.nextLine();
+			
+			System.out.println("Numbers of ours elapsed?");
+			elapsed = scan.nextInt();
+			
+			System.out.println("The time is:  " + (df.format(hour + elapsed) + time));
+
+			
+		
+			
+
+			  
+
+
 		   }
 		
 	}
 
-}
+
