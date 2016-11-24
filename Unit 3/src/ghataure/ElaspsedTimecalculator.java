a16bd7604ba761b53c41559c636de25160216d7c
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-11-24 13:51:11 -0500

    Elapsed Time Calculator

--- Unit 3/src/ghataure/ElaspsedTimecalculator.java
+++ Unit 3/src/ghataure/ElaspsedTimecalculator.java
@@ -21,37 +21,36 @@ public class ElaspsedTimecalculator {
 	 */
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		
+
 		int hour  ;
 		String time;
 		int elapsed;
 		double t2 = 0.00;
-		
-		
 
-		   DecimalFormat df = new DecimalFormat ("0.00");
-			
-			System.out.print("Whats your starting hour?");
-			hour = scan.nextInt();
-			scan.nextLine();
-			
-			System.out.println("Am or Pm?");
-			time = scan.nextLine();
-			
-			System.out.println("Numbers of ours elapsed?");
-			elapsed = scan.nextInt();
-			if (hour+elapsed >=12){
-				System.out.print("Your time is:" +(hour + elapsed - 12) + time);
-			}
-			else if(hour + elapsed <=12){
-				System.out.println("Your time is:" + (hour + elapsed + 12) + time);
-			}
-			
-		
-			
+
+
+		DecimalFormat df = new DecimalFormat ("0.00");
+
+		System.out.print("Whats your starting hour?");
+		hour = scan.nextInt();
+		scan.nextLine();
+
+
+
+		System.out.println("Numbers of ours elapsed?");
+		elapsed = scan.nextInt();
+		if (hour+elapsed >=12){
+			System.out.print("Your time is:" +(hour + elapsed - 12) + "Pm");
+		}
+		else if(hour + elapsed <=12){
+			System.out.println("Your time is:" + (hour + elapsed + 12) + "Am");
+		}
+
+
+
 	}
 }
-		
+
 	
 
 
