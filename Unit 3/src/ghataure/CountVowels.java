06cb3bdc350829b5c2c3140f078e02ce3bf300c0
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-11-30 13:02:27 -0500

    Count vowels

--- Unit 3/src/ghataure/CountVowels.java
+++ Unit 3/src/ghataure/CountVowels.java
@@ -12,16 +12,38 @@ import java.util.Scanner;
 public class CountVowels {
 
 	/**
-	 * @param args
+	 *ICS3U
+	 *Count vowels
+	 *30/11/2016
+	 *Navtej Ghataure
 	 */
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		String text;
-		String str;
-
+		String sentence;
+		int vowels = 0;
+		String a;
+		int i;
 		System.out.println("Enter text:");
-		text=scan.nextLine();
-		System.out.println(text.indexOf(text));
-	}
+		sentence = scan.nextLine();
 
-}
+		for (i = 0; i < sentence.length(); i++){
+
+			if (sentence.charAt(i) == 'a')
+				vowels++;
+			else if (sentence.charAt(i) == 'e')
+				vowels++; 
+			else if (sentence.charAt(i) == 'i')
+				vowels++;
+			else if (sentence.charAt(i) == 'o')
+				vowels++;
+			else if (sentence.charAt(i) == 'u')
+				vowels++;
+
+		}
+		System.out.println("The number of vowels in " + (sentence) + " is " + vowels);
+
+
+
+	}}
+
+
