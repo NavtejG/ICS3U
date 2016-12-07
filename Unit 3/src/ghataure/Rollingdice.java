52aa424aeab41c0f756a0fccb055b9e92c40c631
323878314 <323878314@C-H6304V1.tdsb.on.ca>
2016-12-07 13:13:28 -0500

    Rollingdice

--- Unit 3/src/ghataure/Rollingdice.java
+++ Unit 3/src/ghataure/Rollingdice.java
@@ -18,9 +18,10 @@ public class Rollingdice {
 	 */
 	public static void main(String[] args) {
 
-		        int dice [] = new int[13]; 
+		        int dice [] = new int[12]; 
 		        for(int x = 0 ; x <10000; x++ ) {
-		            ++dice  [(int)(Math.random()*12+1)];
+		            ++dice  [(int)(Math.random()*11+1)];
+		         
 		          
 		        }
 		        System.out.println("Number           Number of times this happened" );  
